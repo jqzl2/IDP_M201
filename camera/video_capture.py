29d57595f697d15b0f9cb5e65ad957c67cf3763d
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-09 13:35:42 +0000

    commit on 'main'

--- camera/video_capture.py
+++ camera/video_capture.py
@@ -8,24 +8,26 @@ cam = cv2.VideoCapture("http://localhost:8081/stream/video.mjpeg")
 if (cam.isOpened() == False):
 	print("Error opening video stream")
 
-# Read until video is completed
 count = 0
-while (cam.isOpened(), count <= 40):
+# Read until video is completed
+while cam.isOpened():
 	# Capture frame-by-frame
-	count += 1
 	ret, frame = cam.read()
 	if ret == True:
 		# Display the resulting frame
-		cv2.imwrite("frame%d.jpg" % count, frame)
 		cv2.imshow('Frame', frame)
 		cv2.waitKey(1)
 
 		# Press Q on keyboard to exit
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			break
+		
+		if cv2.waitKey(25) & 0xFF == ord('s'):
+			cv2.imwrite("frame%d.jpg" % count, frame)
 	
 	# Break the loop
 	else:
+		print("Failed to read camera")
 		break
 
 # When everything done, release the video capture object
