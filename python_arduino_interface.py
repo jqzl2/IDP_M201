96b2d3ce5ff48ba2a24565f68d9410ab3e05099d
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-12 11:56:06 +0000

    wifi

--- python_arduino_interface.py
+++ python_arduino_interface.py
@@ -1,9 +1,20 @@
 import serial #Serial imported for Serial communication
 import time #Required to use delay functions
+import keyboard
 
-ArduinoSerial = serial.Serial('com18',9600)
+arduino = serial.serial_for_url("http://192.168.137.72/")
 time.sleep(2)
-print(ArduinoSerial.readline())
 
-ArduinoSerial.write('1')
+while True:
+    if keyboard.is_pressed('1'):
+        arduino.write(str.encode('1'))
+        print("LED is ON")
+        time.sleep(1)
 
+    if keyboard.is_pressed('0'):
+        arduino.write(str.encode('0'))
+        print("LED is OFF")
+        time.sleep(1)
+    
+    if keyboard.is_pressed('q'):
+        break
