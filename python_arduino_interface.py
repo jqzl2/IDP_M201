09be874c5f8b66b8818d74b982e4355a97433305
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-15 21:33:43 +0000

    just testing

--- python_arduino_interface.py
+++ python_arduino_interface.py
@@ -2,16 +2,15 @@ import urllib3
 import keyboard
 
 arduino = urllib3.PoolManager()
-IP = ''
 
 while True:
     if keyboard.is_pressed('1'):
-        r = arduino.request('GET', 'http://%d/H'%IP)
+        r = arduino.request('GET', 'http://192.168.137.235/H')
         print(r.status)
         print(r.data)
     
     if keyboard.is_pressed('0'):
-        r = arduino.request('GET', 'http://%d/L'%IP)
+        r = arduino.request('GET', 'http://192.168.137.235/L')
         print(r.status)
         print(r.data)
     
