3925bbd3dcaf800d95bdddca839a97cce8c05719
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-15 21:29:38 +0000

    Simple WiFi working

--- python_arduino_interface.py
+++ python_arduino_interface.py
@@ -1,2 +1,21 @@
+import urllib3
+import keyboard
+
+arduino = urllib3.PoolManager()
+IP = ''
+
+while True:
+    if keyboard.is_pressed('1'):
+        r = arduino.request('GET', 'http://%d/H'%IP)
+        print(r.status)
+        print(r.data)
+    
+    if keyboard.is_pressed('0'):
+        r = arduino.request('GET', 'http://%d/L'%IP)
+        print(r.status)
+        print(r.data)
+    
+    if keyboard.is_pressed('q'):
+        break
 
 
