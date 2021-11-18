e4de7da1639c1f560917ef0822e040dfdd6aa0ef
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-18 13:01:39 +0000

    tidying

--- camera/find/findcontours.py
+++ camera/find/findcontours.py
@@ -3,33 +3,12 @@ import numpy as np
 import keyboard
 import glob
 
+from camera.video_capture import undistort
 from numpy.core.numeric import Infinity
 
 DIM=(1016, 760)
 K=np.array([[617.0246864588934, 0.0, 498.84953870958276], [0.0, 620.5390466649645, 395.6528980159782], [0.0, 0.0, 1.0]])
 D=np.array([[0.15842294486041952], [-0.8060115044560319], [1.463190158015197], [-0.9325973032567597]])
-
-def undistort(img):
-	h,w = img.shape[:2]
-
-	diml = img.shape[:2][::-1]
-
-	assert diml[0] / diml[1] == DIM[0] / DIM[1]
-
-	dim2 = diml
-	
-	dim3 = diml
-
-	scaled_K = K * diml[0] / DIM[0]
-	scaled_K[2][2] = 1.0
-
-	balance = 0.0
-
-	new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance)
-	map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
-	undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-
-	return undistorted_img
 
 def isolateCenter(img):
 	img = undistort(img)
