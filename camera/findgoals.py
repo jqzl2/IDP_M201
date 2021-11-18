a45274b3da900556e9926c40e98596fbbe9b7b07
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-18 13:37:50 +0000

    tidying

--- camera/findgoals.py
+++ camera/findgoals.py
@@ -9,11 +9,11 @@ DIM=(1016, 760)
 K=np.array([[617.0246864588934, 0.0, 498.84953870958276], [0.0, 620.5390466649645, 395.6528980159782], [0.0, 0.0, 1.0]])
 D=np.array([[0.15842294486041952], [-0.8060115044560319], [1.463190158015197], [-0.9325973032567597]])
 
-images = glob.glob('D:/George/Documents/GitHub/IDP_M201/camera/contour_testing/*.jpg')
+# images = glob.glob('D:/George/Documents/GitHub/IDP_M201/camera/contour_testing/*.jpg')
 
-for frame in images:
-    findContours(cv2.imread(frame))
-    cv2.waitKey(0)
+# for frame in images:
+#     findContours(cv2.imread(frame))
+#     cv2.waitKey(0)
 
 
 # Create VideoCapture object and read from camera address
