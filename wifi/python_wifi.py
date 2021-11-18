cb090c986970eff531bdecea6282e70bfef16597
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-18 02:14:15 +0000

    lol

--- wifi/python_wifi.py
+++ wifi/python_wifi.py
@@ -8,16 +8,6 @@ while True:
     if keyboard.is_pressed('s'):
         lol = random.randint(0,240)
         r = arduino.request('GET', 'http://192.168.137.131/?lol=%d'%lol)
-
-    # if keyboard.is_pressed('1'):
-    #     r = arduino.request('GET', 'http://192.168.137.217/H')
-    #     print(r.status)
-    #     print(r.data)
-    
-    # if keyboard.is_pressed('0'):
-    #     r = arduino.request('GET', 'http://192.168.137.217/L')
-    #     print(r.status)
-    #     print(r.data)
     
     if keyboard.is_pressed('q'):
         break
