d482bf26341a1d4fdbb5a12b99a476227a626dc0
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-11-19 09:59:37 +0000

    arduino response content changed

--- wifi/arduino_wifi/arduino_wifi.ino
+++ wifi/arduino_wifi/arduino_wifi.ino
@@ -84,8 +84,7 @@ void loop() {
             client.println();
 
             // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> turn the LED on pin 9 on<br>");
-            client.print("Click <a href=\"/L\">here</a> turn the LED on pin 9 off<br>");
+            client.print("Arduino has received PC request");
 
             // The HTTP response ends with another blank line:
             client.println();
